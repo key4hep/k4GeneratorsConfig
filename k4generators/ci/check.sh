3ec3350ec184fecb4b6841bfe1cbd8df5572ca68
Alan Price <alan.price@uni-siegen.de>
2023-12-06 16:05:24 +0100

    Added simple ci

---
+++ k4generators/ci/check.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p test/ref-results
+mkdir -p test/ci-setups
+
+tar xf main-res.tar.gz --directory="${PWD}/test/ref-results"
+REFDIR="${PWD}/test/ref-results"
+
+
+cd test
+cp ref-results/*yaml ci-setups
+cd ci-setups
+
+function checkOutputs() {
+    # TODO: Make this nicer than nested loops
+    for outerDir in *; do
+        if [[ -d "$outerDir" ]]; then
+            for generator in "$outerDir"/*; do
+                if [[ -d "$generator" ]]; then
+                    # echo "Checking $generator"
+                    for outFile in $(ls "$PWD/$generator/"); do
+                        if [[ -e "$REFDIR/$generator/$outFile" ]]; then
+                            echo "Found $outFile in reference results."
+                            if diff "$REFDIR/$generator/$outFile" "$PWD/$generator/$outFile" &> /dev/null; then
+                                echo "Files are identical."
+                            else
+                                echo "Files are different."
+                                exit 1
+                            fi
+                        else
+                            echo "Did not find $outFile in reference results. Not checking!"
+                        fi
+                    done
+                fi
+            done
+        fi
+    done
+}
+
+for yamlFile in *.yaml; do
+    # Extract file name without extension
+    filename="${yamlFile%.yaml}"
+    mkdir -p "test-$filename"
+    cd "test-$filename"
+    echo "Processing file: $yamlFile"
+    python3 ../../../../python/main.py -f "../$yamlFile"
+    checkOutputs
+done
+
+rm -r test
+exit 0
