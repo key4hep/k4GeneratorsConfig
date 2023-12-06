abd91803d517ed84756dfce1356015a9802ad8fc
Dirk Zerwas <Dirk.Zerwas@in2p3.fr>
2023-12-06 13:44:52 +0100

    update to new structure

--- k4generators/python/main.py
+++ k4generators/python/main.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import shutil
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../Tools')))
-sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '../Generators')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'Tools')))
+sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), 'Generators')))
 
 import argparse
 import Input
