22c89c206cf6c08bf4696a5033775d0347bb7b50
Dirk Zerwas <Dirk.Zerwas@in2p3.fr>
2024-04-10 17:03:39 +0200

    correction of reference files

--- test/ref-results/Whizard/Difermion_Polarized/Run_Difermion_Polarized.sh
+++ test/ref-results/Whizard/Difermion_Polarized/Run_Difermion_Polarized.sh
@@ -3,5 +3,5 @@ if [ -z "${KEY4HEP_STACK}" ]; then
     source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 fi
 
-whizard Run_Difermion_Polarized.sin
-$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Difermion_Polarized.edm4hep
+whizard Run_Difermion_Polarized_ISR_BST.sin
+$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Difermion_Polarized_ISR_BST.edm4hep
--- test/ref-results/Whizard/Tau91.2/Run_Tau91.2_ISR.sh
+++ test/ref-results/Whizard/Tau91.2/Run_Tau91.2_ISR.sh
@@ -3,5 +3,5 @@ if [ -z "${KEY4HEP_STACK}" ]; then
     source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 fi
 
-whizard Run_Tau91.2.sin
-$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Tau91.2.edm4hep
+whizard Run_Tau91.2_ISR.sin
+$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Tau91.2_ISR.edm4hep
