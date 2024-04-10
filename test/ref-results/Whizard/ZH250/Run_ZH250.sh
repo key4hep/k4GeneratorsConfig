c293fc4e3f282eaf7af5d6d7738a6aa1b02713fd
Dirk Zerwas <Dirk.Zerwas@in2p3.fr>
2024-04-10 21:03:34 +0200

    complete missing ref

--- test/ref-results/Whizard/ZH250/Run_ZH250.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ -z "${KEY4HEP_STACK}" ]; then
-    source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
-fi
-
-whizard Run_ZH250.sin
-$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc ZH250.edm4hep
