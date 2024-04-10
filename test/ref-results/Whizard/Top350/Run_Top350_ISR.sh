9f2be465c1d1706dad12ca57d4b1437f347c8b56
Dirk Zerwas <Dirk.Zerwas@in2p3.fr>
2024-04-10 17:29:42 +0200

    correction of reference files

--- test/ref-results/Whizard/Top350/Run_Top350_ISR.sh
+++ test/ref-results/Whizard/Top350/Run_Top350_ISR.sh
@@ -3,5 +3,5 @@ if [ -z "${KEY4HEP_STACK}" ]; then
     source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 fi
 
-whizard Run_Top350.sin
-$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Top350.edm4hep
+whizard Run_Top350_ISR.sin
+$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Top350_ISR.edm4hep
