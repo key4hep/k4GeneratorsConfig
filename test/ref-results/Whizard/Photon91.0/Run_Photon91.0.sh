61980084af6d565531a003ba9d1a64e6dc9bb62c
Dirk Zerwas <Dirk.Zerwas@in2p3.fr>
2024-04-25 11:14:26 +0200

    complete ref for Photon prod

---
+++ test/ref-results/Whizard/Photon91.0/Run_Photon91.0.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ -z "${KEY4HEP_STACK}" ]; then
+    source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+fi
+
+whizard Photon91.0.sin
+$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Photon91.0.edm4hep
