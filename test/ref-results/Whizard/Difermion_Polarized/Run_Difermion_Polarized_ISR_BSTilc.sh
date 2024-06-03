f242f3cb2c372b38f9b0715756a21c7cfd9c84e0
Dirk Zerwas <Dirk.Zerwas@in2p3.fr>
2024-06-03 18:06:27 +0200

    finalize ref file

--- test/ref-results/Whizard/Difermion_Polarized/Run_Difermion_Polarized_ISR_BSTilc.sh
+++ test/ref-results/Whizard/Difermion_Polarized/Run_Difermion_Polarized_ISR_BSTilc.sh
@@ -3,5 +3,5 @@ if [ -z "${KEY4HEP_STACK}" ]; then
     source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 fi
 
-whizard Difermion_Polarized_ISR_BST.sin
-$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Difermion_Polarized_ISR_BST.edm4hep
+whizard Difermion_Polarized_ISR_BSTilc.sin
+$CONVERTHEPMC2EDM4HEP/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep proc.hepmc Difermion_Polarized_ISR_BSTilc.edm4hep
