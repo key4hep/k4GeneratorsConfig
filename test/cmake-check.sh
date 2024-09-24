53cadb6d17cb01767a5b17f4098f09d2cc53b1f5
Dirk Zerwas <148551698+dirkzerwas@users.noreply.github.com>
2024-09-24 11:22:05 +0200

    Update cmake-check.sh
    remove setup (parallelization problem)

--- test/cmake-check.sh
+++ test/cmake-check.sh
@@ -40,24 +40,8 @@ done
 
 
 CWD=${PWD}
-# only create the directory if it does not exist yet
-if [[ ! -d ${CWD}/ci-setups ]]; then
-   mkdir -p ${CWD}/ci-setups
-fi
-
 REFDIR="${PWD}/ref-results"
 EXAMPLEDIR="${PWD}/../examples"
-
-# only copy if the file does not exist yet:
-for yamlFileWithPath in "$EXAMPLEDIR"/FermionProduction.*yaml; do
-   yamlFile="$(basename "$yamlFileWithPath")"
-   echo checking for ci-setups/"$yamlFile"
-   if [[ ! -f ci-setups/"$yamlFile" ]]; then
-      echo copying $yamlFileWithPath to ci-setups
-      cp -f "$yamlFileWithPath" ci-setups
-      ls -l ci-setups/"$yamlFile"
-   fi
-done
 cd ci-setups
 
 function checkFile() {
