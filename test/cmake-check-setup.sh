f386637c79252fc1696fc8b1c8dac35cd117b441
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2024-09-24 11:44:11 +0200

    separate yaml processing from running

--- test/cmake-check-setup.sh
+++ test/cmake-check-setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
+shopt -s expand_aliases
+source ../setup.sh
 
 CWD=${PWD}
 # only create the directory if it does not exist yet
@@ -19,5 +21,57 @@ for yamlFileWithPath in "$EXAMPLEDIR"/FermionProduction.*yaml; do
       cp -f "$yamlFileWithPath" ci-setups
    fi
 done
+
+cd ci-setups
+
+# STEP 1: check the input
+
+for yamlFile in *.yaml; do
+    processYAML "$yamlFile"
+done
+
+function processYAML() {
+    local yamlFile="$1"
+    local filename="${yamlFile%.yaml}"
+
+    mkdir -p "test-$filename"
+    cd "test-$filename"
+    echo "Processing file: $yamlFile"
+    k4gen -f "../$yamlFile" --nevts 100
+    checkOutputs
+    cd ..
+}
+
+function checkOutputs() {
+    # for generator in */*; do
+    # if [[ -d "$generator" ]] || continue
+    echo "Checking $generator"
+    for outFile in "$PWD/$generator"/*/*; do
+        [[ -f "$outFile" ]] || continue
+    local fullpath="$(dirname "$outFile")"
+    local procname="$(basename "$fullpath")"
+        checkFile "$generator" "$procname" "$(basename "$outFile")"
+        # done
+    done
+}
+
+function checkFile() {
+    local generator="$1"
+    local refgenerator="$(basename "$generator")"
+    local procname="$2"
+    local outFile="$3"
+    if [[ -e "$REFDIR/$refgenerator/$procname/$outFile" ]]; then
+        if diff "$REFDIR/$refgenerator/$procname/$outFile" "$PWD/$generator/$procname/$outFile" &> /dev/null; then
+            echo "Process " $procname : "Files are identical for file" $outFile 
+        else
+            echo "Process " $procname "Files are different for file" $outFile 
+            diff "$REFDIR/$refgenerator/$procname/$outFile" "$PWD/$generator/$procname/$outFile"
+            exit 1
+        fi
+    else
+        echo "Did not find $outFile. Not checking!"
+    fi
+}
+
 
 exit 0
--- test/cmake-check.sh
+++ test/cmake-check.sh
@@ -44,55 +44,6 @@ REFDIR="${PWD}/ref-results"
 EXAMPLEDIR="${PWD}/../examples"
 cd ci-setups
 
-function checkFile() {
-    local generator="$1"
-    local refgenerator="$(basename "$generator")"
-    local procname="$2"
-    local outFile="$3"
-    if [[ -e "$REFDIR/$refgenerator/$procname/$outFile" ]]; then
-        if diff "$REFDIR/$refgenerator/$procname/$outFile" "$PWD/$generator/$procname/$outFile" &> /dev/null; then
-            echo "Process " $procname : "Files are identical for file" $outFile 
-        else
-            echo "Process " $procname "Files are different for file" $outFile 
-            diff "$REFDIR/$refgenerator/$procname/$outFile" "$PWD/$generator/$procname/$outFile"
-            exit 1
-        fi
-    else
-        echo "Did not find $outFile. Not checking!"
-    fi
-}
-
-function checkOutputs() {
-    # for generator in */*; do
-    # if [[ -d "$generator" ]] || continue
-    echo "Checking $generator"
-    for outFile in "$PWD/$generator"/*/*; do
-        [[ -f "$outFile" ]] || continue
-    local fullpath="$(dirname "$outFile")"
-    local procname="$(basename "$fullpath")"
-        checkFile "$generator" "$procname" "$(basename "$outFile")"
-        # done
-    done
-}
-
-function processYAML() {
-    local yamlFile="$1"
-    local filename="${yamlFile%.yaml}"
-
-    mkdir -p "test-$filename"
-    cd "test-$filename"
-    echo "Processing file: $yamlFile"
-    k4gen -f "../$yamlFile" --nevts 100
-    checkOutputs
-    cd ..
-}
-
-# STEP 1: check the input
-
-for yamlFile in *.yaml; do
-    processYAML "$yamlFile"
-done
-
 # STEP 2: run the generators
 
 function processRun() {
