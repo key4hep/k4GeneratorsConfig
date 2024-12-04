e61341cc325a5fa1c09e1bf4d25232de2163ac1e
Dirk Zerwas <148551698+dirkzerwas@users.noreply.github.com>
2024-12-04 16:46:14 +0100

    GeneratorDBLabel with Initial State

--- python/Generators/PythiaProcDB.py
+++ python/Generators/PythiaProcDB.py
@@ -35,39 +35,37 @@ class PythiaProcDB:
 
         # choose as function of generatorDBLabel
         label = self.process.get_generatorDBLabel()
-        if label == "1_1":
+        if label == "11_11_1_1":
             self.write_Difermion(1)
-        elif label == "2_2":
+        elif label == "11_11_2_2":
             self.write_Difermion(2)
-        elif label == "3_3":
+        elif label == "11_11_3_3":
             self.write_Difermion(3)
-        elif label == "4_4":
+        elif label == "11_11_4_4":
             self.write_Difermion(4)
-        elif label == "5_5":
+        elif label == "11_11_5_5":
             self.write_Difermion(5)
-        elif label == "6_6":
+        elif label == "11_11_6_6":
             self.write_Ditop()
-        elif label == "12_12":
+        elif label == "11_11_12_12":
             self.write_Difermion(12)
-        elif label == "13_13":
+        elif label == "11_11_13_13":
             self.write_Difermion(13)
-        elif label == "14_14":
+        elif label == "11_11_14_14":
             self.write_Difermion(14)
-        elif label == "15_15":
+        elif label == "11_11_15_15":
             self.write_Difermion(15)
-        elif label == "16_16":
+        elif label == "11_11_16_16":
             self.write_Difermion(16)
-        elif label == "22_22":
+        elif label == "11_11_22_22":
             self.write_Diphoton()
-        elif label == "23_23":
+        elif label == "11_11_23_23":
             self.write_ZZ()
-        elif label == "24_24":
+        elif label == "11_11_24_24":
             self.write_WW()
-        elif label == "23_25":
+        elif label == "11_11_23_25":
             self.write_run_ZH()
-        elif label == "25_23":
-            self.write_run_ZH()
-        elif label == "12_12_25":
+        elif label == "11_11_12_12_25":
             self.write_run_Hnunu()
             
     def write_Difermion(self, pdg):
