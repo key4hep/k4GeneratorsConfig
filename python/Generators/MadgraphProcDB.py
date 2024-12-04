bba3743c86a606f3b99a893dd8722467be76480a
Dirk Zerwas <148551698+dirkzerwas@users.noreply.github.com>
2024-12-04 16:54:30 +0100

    Make the initial state e+e- to be safe

--- python/Generators/MadgraphProcDB.py
+++ python/Generators/MadgraphProcDB.py
@@ -8,7 +8,7 @@ class MadgraphProcDB:
 
     def write_DBInfo(self):
         # choose as function of generatorDBLabel
-        if len(self.process.final) == 2:
+        if self.generatorDBLabel.startswith("11_11") and len(self.process.final) == 2 :
             if abs(self.process.final[0]) <= 16:
                 self.runout += self.write_Difermion()
         label = self.process.get_generatorDBLabel()
