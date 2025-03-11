5b90906e19f68fa3de1d1dd6f831dbc42c4fc491
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-03-11 16:25:41 +0100

    update ref for configuration

--- test/ref-results/Sherpa/ZHDecay350/Run_ZHDecay350.sh
+++ test/ref-results/Sherpa/ZHDecay350/Run_ZHDecay350.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 if [ -z "${KEY4HEP_STACK}" ]; then
     source /cvmfs/sw.hsf.org/key4hep/setup.sh
+    if [ -z "${K4GenBuildDir}" ]; then
+        export K4GenBuildDir=${K4GENERATORSCONFIG}/../../
+        echo "variable K4GenBuildDir was not defined using directory ${K4GenBuildDir} for the executables"
+    else
+        echo "k4GeneratorsConfig:: using directory ${K4GenBuildDir} for the executables"
+    fi
 fi
 
 Sherpa -f ZHDecay350.dat
