e23de9fac674a808b47ddffa02718623d8cbe22a
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-04-11 18:39:47 +0200

    remove trailing blanc

--- test/ref-results/Babayaga/Photon91.0/Run_Photon91.0.sh
+++ test/ref-results/Babayaga/Photon91.0/Run_Photon91.0.sh
@@ -13,4 +13,4 @@ cat Photon91.0.dat | babayaga-fcc.exe
 $K4GenBuildDir/bin/convertHepMC2EDM4HEP -i lhe -o hepmc3 events.lhe Photon91.0.hepmc3
 $K4GenBuildDir/bin/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep Photon91.0.hepmc3 Photon91.0.edm4hep
 
-$K4GenBuildDir/bin/key4HEPAnalysis -i Photon91.0.edm4hep -o Photon91.0.root -p 22,22 
+$K4GenBuildDir/bin/key4HEPAnalysis -i Photon91.0.edm4hep -o Photon91.0.root -p 22,22
