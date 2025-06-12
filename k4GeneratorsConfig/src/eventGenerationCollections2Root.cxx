85c3b50bd56c84117ca1062fea259b2c1156335f
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-06-12 11:09:39 +0200

    clang

--- k4GeneratorsConfig/src/eventGenerationCollections2Root.cxx
+++ k4GeneratorsConfig/src/eventGenerationCollections2Root.cxx
@@ -450,25 +450,25 @@ void k4GeneratorsConfig::eventGenerationCollections2Root::writeAnalysisHistosFig
         if (obj->InheritsFrom(TH1D::Class())) {
           // recreate TH1D
           TH1D* generatorHisto = new TH1D(*(TH1D*)obj);
-	  // create the average histo
-	  if ( generatorAverageHisto == nullptr ) {
-	    name << generatorHisto->GetName() << counter;
-	    generatorAverageHisto = new TH1D(name.str().c_str(), generatorHisto->GetTitle(), generatorHisto->GetNbinsX(),
-				       generatorHisto->GetBinLowEdge(1),
-				       generatorHisto->GetBinLowEdge(generatorHisto->GetNbinsX() + 1));
-	    generatorAverageHisto->GetXaxis()->SetTitle(generatorHisto->GetXaxis()->GetTitle());
-	    name.clear();
-	    name.str("");
-	  }
-	  if (!(generatorAverageHisto->GetSumw2N() > 0))
-	    generatorAverageHisto->Sumw2(kTRUE);
-	  generatorAverageHisto->Add(generatorHisto);
-	  counter++;
-	}
+          // create the average histo
+          if (generatorAverageHisto == nullptr) {
+            name << generatorHisto->GetName() << counter;
+            generatorAverageHisto = new TH1D(name.str().c_str(), generatorHisto->GetTitle(),
+                                             generatorHisto->GetNbinsX(), generatorHisto->GetBinLowEdge(1),
+                                             generatorHisto->GetBinLowEdge(generatorHisto->GetNbinsX() + 1));
+            generatorAverageHisto->GetXaxis()->SetTitle(generatorHisto->GetXaxis()->GetTitle());
+            name.clear();
+            name.str("");
+          }
+          if (!(generatorAverageHisto->GetSumw2N() > 0))
+            generatorAverageHisto->Sumw2(kTRUE);
+          generatorAverageHisto->Add(generatorHisto);
+          counter++;
+        }
       }
       // we got all our histos together, so we can average
-      if ( counter > 0 ) {
-	generatorAverageHisto->Scale(1. / counter);
+      if (counter > 0) {
+        generatorAverageHisto->Scale(1. / counter);
       }
       // push_back
       analysisHistosAverage[proc].push_back(generatorAverageHisto);
@@ -494,12 +494,12 @@ void k4GeneratorsConfig::eventGenerationCollections2Root::writeAnalysisHistosFig
           TH1D* theDelta = new TH1D(*(TH1D*)obj);
           if (!(theDelta->GetSumw2N() > 0))
             theDelta->Sumw2(kTRUE);
-	  // check the compatibility of the histo compared to average
-	  calculateChi2(m_processesSqrtsList[proc], theDelta, analysisHistosAverage[proc][ihisto]);
-	  // subtract average and divide
+          // check the compatibility of the histo compared to average
+          calculateChi2(m_processesSqrtsList[proc], theDelta, analysisHistosAverage[proc][ihisto]);
+          // subtract average and divide
           theDelta->Add(analysisHistosAverage[proc][ihisto], -1.);
           theDelta->Divide(analysisHistosAverage[proc][ihisto]);
-	  // draw the histo and set the usual options
+          // draw the histo and set the usual options
           theDelta->Draw("SAME");
           theDelta->GetXaxis()->SetTitle(analysisHistosAverage[proc][ihisto]->GetXaxis()->GetTitle());
           theDelta->GetXaxis()->SetTitleSize(0.12);
@@ -519,25 +519,27 @@ void k4GeneratorsConfig::eventGenerationCollections2Root::writeAnalysisHistosFig
   }
 }
 void k4GeneratorsConfig::eventGenerationCollections2Root::writeTree() { m_tree->Write(); }
-void k4GeneratorsConfig::eventGenerationCollections2Root::calculateChi2(std::string procName, TH1D* histo1, TH1D* histo2) {
+void k4GeneratorsConfig::eventGenerationCollections2Root::calculateChi2(std::string procName, TH1D* histo1,
+                                                                        TH1D* histo2) {
 
-  if ( histo1->GetNbinsX() != histo2->GetNbinsX() ) {
-    std::cout << "Process " << procName << " chi2 test impossible: number of bins incompatible " << histo1->GetNbinsX() << " != " << histo2->GetNbinsX() << std::endl;
+  if (histo1->GetNbinsX() != histo2->GetNbinsX()) {
+    std::cout << "Process " << procName << " chi2 test impossible: number of bins incompatible " << histo1->GetNbinsX()
+              << " != " << histo2->GetNbinsX() << std::endl;
   }
   double chi2 = 0.;
   unsigned int nbOfPoints = 0;
-  for (int i = 0; i < histo1->GetNbinsX()+2; i++) {
-    if ( histo1->GetBinError(i) != 0. || histo2->GetBinError(i) != 0. ) {
+  for (int i = 0; i < histo1->GetNbinsX() + 2; i++) {
+    if (histo1->GetBinError(i) != 0. || histo2->GetBinError(i) != 0.) {
       double deltaChi2 = histo1->GetBinContent(i) - histo2->GetBinContent(i);
       deltaChi2 *= deltaChi2;
-      deltaChi2 /= (histo1->GetBinError(i)*histo1->GetBinError(i) + histo2->GetBinError(i)*histo2->GetBinError(i));
+      deltaChi2 /= (histo1->GetBinError(i) * histo1->GetBinError(i) + histo2->GetBinError(i) * histo2->GetBinError(i));
       chi2 += deltaChi2;
       nbOfPoints++;
     }
   }
 
   // divide by the number of measurements
-  chi2 /= nbOfPoints;  
-  std::cout << "Proc " << procName << " Generator " << histo1->GetTitle() << " Type " << histo2->GetXaxis()->GetTitle() << " chi2 = " << chi2 << std::endl; 
+  chi2 /= nbOfPoints;
+  std::cout << "Proc " << procName << " Generator " << histo1->GetTitle() << " Type " << histo2->GetXaxis()->GetTitle()
+            << " chi2 = " << chi2 << std::endl;
 }
-
