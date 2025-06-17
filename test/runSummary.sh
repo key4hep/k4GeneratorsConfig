3bd7231d41771eebe7f85ada5e5167f597cebf49
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-06-17 16:31:53 +0200

    debug whizard on alma

--- test/runSummary.sh
+++ test/runSummary.sh
@@ -13,15 +13,17 @@ cd ci-setups
 echo Extracting the cross sections by reading EDM4HEP files and superposing the differential distributions
 $K4GENERATORSCONFIG/eventGenerationSummary -f ${CWD}/GenerationSummary.dat
 
-for file in *.png *.pdf *.root;
-do
-  if
-    [-f "$file"];
-then cp $file ${CWD} / output /
-                           .fi done
+for file in *.png *.pdf *.root; do
+    if [ -f "$file"] ; then
+	cp $file ${CWD}/output/
+    fi
+done
 
-                               cat ${CWD} /
-    GenerationSummary.dat
+cat ${CWD}/GenerationSummary.dat
+
+echo Debugging WHizard on alma
+ls -l $(CWD)/ci-setups/test-FermionProduction/Run-Cards/Whizard/Muon350/
+cat $(CWD)/ci-setups/test-FermionProduction/Run-Cards/Whizard/Muon350/whizard.log
 
 #cleanup at the end
 #rm - r ${CWD } / ci - setups
