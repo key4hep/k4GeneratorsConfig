70f87ccf8fc9e48431c31080c5a5f7579fa78e99
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-06-17 19:13:15 +0200

    debug whizard

--- test/runSummary.sh
+++ test/runSummary.sh
@@ -21,16 +21,26 @@ done
 
 cat ${CWD}/GenerationSummary.dat
 
-echo Debugging WHizard on alma
+echo Debugging WHizard on alma  >> ${CWD}/GenerationSummary.dat
 ls -l ${CWD}/ci-setups/test-FermionProduction/Run-Cards/Whizard/Muon350/ >> ${CWD}/GenerationSummary.dat
 cat ${CWD}/ci-setups/test-FermionProduction/Run-Cards/Whizard/Muon350/whizard.log >> ${CWD}/GenerationSummary.dat
-echo " "
-echo ls /cvmfs/sw-nightlies.hsf.org/key4hep/releases/2025-06-07/x86_64-almalinux9-gcc14.2.0-opt/whizard/3.1.5-3v5fcx/lib/libomega.so.0.0.0
-ls -l /cvmfs/sw-nightlies.hsf.org/key4hep/releases/2025-06-07/x86_64-almalinux9-gcc14.2.0-opt/whizard/3.1.5-3v5fcx/lib/libomega.so.0.0.0 >> ${CWD}/GenerationSummary.dat
+echo " "  >> ${CWD}/GenerationSummary.dat
+echo ls /cvmfs/sw-nightlies.hsf.org/key4hep/releases/2025-06-07/x86_64-almalinux9-gcc14.2.0-opt/whizard/3.1.5-3v5fcx/lib/libomega.so.0  >> ${CWD}/GenerationSummary.dat
+ls -l /cvmfs/sw-nightlies.hsf.org/key4hep/releases/2025-06-07/x86_64-almalinux9-gcc14.2.0-opt/whizard/3.1.5-3v5fcx/lib/libomega.so.0 >> ${CWD}/GenerationSummary.dat
 ls -l /cvmfs/sw-nightlies.hsf.org/key4hep/releases/2025-06-07/x86_64-almalinux9-gcc14.2.0-opt/whizard/3.1.5-3v5fcx/lib/* >> ${CWD}/GenerationSummary.dat
 echo " "
 echo COmpiler listing:
 cat ${CWD}/ci-setups/test-FermionProduction/Run-Cards/Whizard/Muon350/default_lib.makefile >> ${CWD}/GenerationSummary.dat
+
+echo "GOING TO THE LIBRARY"  >> ${CWD}/GenerationSummary.dat
+cd /cvmfs/sw.hsf.org/key4hep/releases/2025-05-29/x86_64-almalinux9-gcc14.2.0-opt/whizard/3.1.4-zjmc7r/lib
+echo "ls -l "  >> ${CWD}/GenerationSummary.dat
+ls -l  >> ${CWD}/GenerationSummary.dat
+echo "ldd libomega.so"  >> ${CWD}/GenerationSummary.dat
+ldd libomega.so  >> ${CWD}/GenerationSummary.dat
+echo "ldd libomega.so.0"  >> ${CWD}/GenerationSummary.dat
+ldd libomega.so.0  >> ${CWD}/GenerationSummary.dat
+
 #cleanup at the end
 #rm - r ${CWD } / ci - setups
 
