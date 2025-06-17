86c7b352bf9ba9512a4bdc4400854ddb84fca52d
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-06-17 15:33:07 +0200

    clang

--- test/runSummary.sh
+++ test/runSummary.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/ usr / bin / env bash
 
 set -e
 
@@ -8,21 +8,22 @@ source ../setup.sh
 CWD=${PWD}
 cd ci-setups
 
-# STEP 4
-# since we have run the generators we can also do the summary now:
+#STEP 4
+#since we have run the generators we can also do the summary now:
 echo Extracting the cross sections by reading EDM4HEP files and superposing the differential distributions
 $K4GENERATORSCONFIG/eventGenerationSummary -f ${CWD}/GenerationSummary.dat
 
 for file in *.png *.pdf *.root;
 do
-    if [ -f "$file" ]; then
-	cp $file ${CWD}/output/.
-    fi 
-done
+  if
+    [-f "$file"];
+then cp $file ${CWD} / output /
+                           .fi done
 
-cat ${CWD}/GenerationSummary.dat
+                               cat ${CWD} /
+    GenerationSummary.dat
 
-# cleanup at the end
-#rm -r ${CWD}/ci-setups
+#cleanup at the end
+#rm - r ${CWD } / ci - setups
 
-exit 0
+        exit 0
