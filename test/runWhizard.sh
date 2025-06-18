72ecbd800b31c7d98b0ddc2719f2abdc4d626b4c
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-06-18 15:30:40 +0200

    debug whizard ci

--- test/runWhizard.sh
+++ test/runWhizard.sh
@@ -6,8 +6,9 @@ export MYLIB=`whizard-config --libdir`
 echo lib dir is $MYLIB >> whizardDEBUG.log
 echo ldd $MYLIB/../libomega.so.0 >> whizardDEBUG.log
 ldd $MYLIB/../libomega.so.0 >> whizardDEBUG.log
-echo LOOKING INTO .libs
-ls .libs/* >> whizardDEBUG.log
 whizard ref-results/Whizard/Muon350/Muon350_ISR.sin
+echo LOOKING INTO .libs >> whizardDEBUG.log
+ls .libs/* >> whizardDEBUG.log
+echo running ls -alt :
 ls -alt >> whizardDEBUG.log
 
