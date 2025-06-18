b2fd167d9ab10d867d2259d94685cbdeca2103fe
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-06-18 15:43:20 +0200

    debug whizard ci

--- test/runWhizard.sh
+++ test/runWhizard.sh
@@ -7,8 +7,14 @@ echo lib dir is $MYLIB >> whizardDEBUG.log
 echo ldd $MYLIB/../libomega.so.0 >> whizardDEBUG.log
 ldd $MYLIB/../libomega.so.0 >> whizardDEBUG.log
 whizard ref-results/Whizard/Muon350/Muon350_ISR.sin
+echo LIBTOOL in makefile is: >> whizardDEBUG.log
+grep libtool default_lib.makefile >> whizardDEBUG.log
 echo LOOKING INTO .libs >> whizardDEBUG.log
 ls .libs/* >> whizardDEBUG.log
-echo running ls -alt :
-ls -alt >> whizardDEBUG.log
+echo ldd of .libs/default_lib.so
+ldd .libs/default_lib.so
+echo ldd of .libs/default_lib.so.0
+ldd .libs/default_lib.so.0
+echo ldd of .libs/default_lib.so.0.0.0
+ldd .libs/default_lib.so.0.0.0
 
