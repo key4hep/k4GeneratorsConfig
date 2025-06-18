69eed395432e0c10058bc1a21455680fbf3c4c97
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2025-06-18 13:39:54 +0200

    debug whizard ci

--- test/runWhizard.sh
+++ test/runWhizard.sh
@@ -1,4 +1,2 @@
-which libtool
-export MYLIBTOOL=`/cvmfs/sw-nightlies.hsf.org/key4hep/releases/2025-06-07/x86_64-almalinux9-gcc14.2.0-opt/whizard/3.1.5-3v5fcx/bin/whizard-config --libdir`/libtool
-echo libtool is set to $MYLIBTOOL
-whizard ref-results/Whizard/Muon350/Muon350_ISR.sin --libtool $MYLIBTOOL
+whizard ref-results/Whizard/Muon350/Muon350_ISR.sin
+ls -alt
