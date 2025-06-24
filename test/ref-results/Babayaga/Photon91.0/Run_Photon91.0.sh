eb4cd3bd8adbc581ee368dda7c4830b3a4d2a84b
Dirk Zerwas <148551698+dirkzerwas@users.noreply.github.com>
2025-06-24 20:01:45 +0200

    safer multienvironment

--- test/ref-results/Babayaga/Photon91.0/Run_Photon91.0.sh
+++ test/ref-results/Babayaga/Photon91.0/Run_Photon91.0.sh
@@ -2,12 +2,9 @@
 if [ -z "${KEY4HEP_STACK}" ]; then
     source /cvmfs/sw.hsf.org/key4hep/setup.sh
 fi
-if [ -z "${K4GeneratorsConfigBinDir}" ]; then
-   K4GeneratorsConfigBinDir=""
-fi
 
 cat Photon91.0.dat | babayaga-fcc.exe
-${K4GeneratorsConfigBinDir}convertHepMC2EDM4HEP -i lhe -o hepmc3 events.lhe Photon91.0.hepmc3
-${K4GeneratorsConfigBinDir}convertHepMC2EDM4HEP -i hepmc3 -o edm4hep Photon91.0.hepmc3 Photon91.0.edm4hep
+${$K4GENERATORSCONFIG}/convertHepMC2EDM4HEP -i lhe -o hepmc3 events.lhe Photon91.0.hepmc3
+${$K4GENERATORSCONFIG}/convertHepMC2EDM4HEP -i hepmc3 -o edm4hep Photon91.0.hepmc3 Photon91.0.edm4hep
 
-${K4GeneratorsConfigBinDir}key4HEPAnalysis -i Photon91.0.edm4hep -o Photon91.0.root -p 22,22
+${$K4GENERATORSCONFIG}/key4HEPAnalysis -i Photon91.0.edm4hep -o Photon91.0.root -p 22,22
