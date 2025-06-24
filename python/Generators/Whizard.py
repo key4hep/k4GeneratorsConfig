72e87f716e35cab5502d593f67a9438c722be1ff
Dirk Zerwas <148551698+dirkzerwas@users.noreply.github.com>
2025-06-24 19:58:26 +0200

    safer multienvironment

--- python/Generators/Whizard.py
+++ python/Generators/Whizard.py
@@ -182,7 +182,7 @@ class Whizard(GeneratorBase):
             key4hepRun += f"wget https://whizard.hepforge.org/circe_files/{accel}/{self.procinfo.get_BeamstrahlungFile()}\n"
         # back to normal
         key4hepRun += self.executable + " " + self.GeneratorDatacardName + "\n"
-        key4hepRun += "{0}convertHepMC2EDM4HEP -i {1} -o edm4hep proc.hepmc {2}.edm4hep\n".format(
+        key4hepRun += "{0}/convertHepMC2EDM4HEP -i {1} -o edm4hep proc.hepmc {2}.edm4hep\n".format(
             self.binDir, self.procinfo.get("output_format"), self.GeneratorDatacardBase
         )
         self.add2Key4hepScript(key4hepRun)
