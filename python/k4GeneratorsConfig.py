eef084dbb52b9d56761aa5e8e5fca073bcba1bb0
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2026-04-08 14:02:04 +0200

    warnings for incompatible options

--- python/k4GeneratorsConfig.py
+++ python/k4GeneratorsConfig.py
@@ -14,59 +14,76 @@ from Production import summary
 
 class k4GeneratorsConfig():
     def __init__(self,arguments=None):
+        # define all command line arguments
         parser = argparse.ArgumentParser(prog="k4GeneratorsConfig")
+        
         parser.add_argument(
             "--make",
             action='store_true',
             help="make the generator datacards from the yaml files"
         )
+
+        self.yamlDefault = [os.path.dirname(os.path.realpath(__file__))+'/../examples']
         parser.add_argument(
             "--yaml",
             nargs="*",
             type=str,
-            default=[os.path.dirname(os.path.realpath(__file__))+'/../examples'],
+            default=argparse.SUPPRESS,
             help="yamlFiles and director(y/ies) with yaml files (default: k4GeneratorsConfig/examples)"
         )
+
+        self.sqrtsDefault = []
         parser.add_argument(
             "--sqrts",
             nargs="*",
             type=str,
-            default=[],
+            default=argparse.SUPPRESS,
             help="either a space separated list of center of mass energies OR file(s) and director(y/ies) with sqrts lists in yaml format (name : sqrtsPROCESS.dat containing eg, sqrts:[91.,240.]),  sqrts.yaml as single argument will be applied to all processes",
         )
+
+        self.seedDefault = 4711
         parser.add_argument(
             "--seed",
             nargs=1,
             type=int,
-            default=4711,
-            help="initial random number seed, increment for each file",
+            default=argparse.SUPPRESS,
+            help="If specified overrides the random number seed in the yamlFile, incremented for each process and each sqrts of a yaml file, default: yaml file",
         )
+
+        self.nevtsDefault = -1
         parser.add_argument(
             "--nevts",
             type=int,
-            default=-1,
-            help="Number of events to be generated",
+            default=argparse.SUPPRESS,
+            help="If specified overrides the number of events to be generated in the yamlFile, default: use yaml",
         )
+
+        self.parameterTagDefault = "latest"
         parser.add_argument(
             "--parameterTag",
             type=str,
-            default="latest",
+            default=argparse.SUPPRESS,
             help="parameter tag in Parameters.yaml (default: latest)",
         )
+
+        self.parameterTagFileDefault = os.path.dirname(os.path.realpath(__file__))+'/ParameterSets.yaml'
         parser.add_argument(
             "--parameterTagFile",
             type=str,
-            default=os.path.dirname(os.path.realpath(__file__))+'/ParameterSets.yaml',
+            default=argparse.SUPPRESS,
             help="name of file containing the parameter sets of the requested parameterTag, default: ParameterSets.yaml in directory: k4GeneratorsConfig/python",
         )
+
         parser.add_argument(
             "--key4hepUseNightlies",
             action='store_true',
             help="configures the key4hepscripts to use nightlies instead of releases",
         )
+
+        self.key4hepVersionDefault = None
         parser.add_argument(
             "--key4hepVersion",
-            default=None,
+            default=argparse.SUPPRESS,
             help="force the use of the version : YYYY-MM-DD (default: latest)",
         )
         parser.add_argument(
@@ -96,6 +113,7 @@ class k4GeneratorsConfig():
             action='store_true',
             help="compare the results of the event generation process by process and produce summary output in outputDir"
         )
+
         self.outputDirDefault = "work"
         parser.add_argument(
             "--outputDir",
@@ -132,7 +150,7 @@ class k4GeneratorsConfig():
         # OPTION: outputDir
         # differentiate between option being given or not
         try:
-            check = self.args.outputDir
+            check = args.outputDir
             self.args.outputDirOverride = True
         except AttributeError:
             # argument was not given, set the default and make it known:
@@ -140,7 +158,7 @@ class k4GeneratorsConfig():
             self.args.outputDirOverride = False
 
         # --all overrides --make --generate --summary
-        if self.args.all:
+        if args.all:
             self.args.make     = True
             self.args.generate = True
             self.args.summary  = True
@@ -152,16 +170,72 @@ class k4GeneratorsConfig():
             sys.exit(message)
 
         # OPTION all or make&&generate&&summary
-        if ( self.args.all or
-             (self.args.make and self.args.check) or
-             (self.args.make and self.args.generate) or
-             (self.args.make and self.args.summary) ):
+        if ( args.all or
+             (args.make and args.check) or
+             (args.make and args.generate) or
+             (args.make and args.summary) ):
             if not self.args.outputDirOverride:
                 message = f"k4GeneratorsConfig::ERROR\n"
                 message += f"--make and (--check and/or --generate and/or --summary) requested\n"
                 message += f"yamlFiles may define multiple outputDirectory, functionality not foreseen\n"
                 message += f"BUT: --outputDir {self.args.outputDir} not defined \nPlease define a common output directory"
                 sys.exit(message)
+
+        # make sure that Warnings are issued when options specific to --make are invoked, make is false
+        message = "k4GeneratorsConfig::WARNING invoking option specific to --make or --all:"
+        try:
+            check = self.args.yaml
+            if not args.make and not args.all:
+                print(f"{message} --yaml {self.args.yaml} has no effect")
+        except AttributeError as e:
+            if args.make or args.all:
+                self.args.yaml = self.yamlDefault
+        try:
+            check = self.args.sqrts
+            if not args.make and not args.all:
+                print(f"{message} --sqrts {self.args.sqrts} has no effect")
+        except AttributeError as e:
+            if args.make or args.all:
+                self.args.sqrts = self.sqrtsDefault
+        try:
+            check = self.args.seed
+            if not args.make and not args.all:
+                print(f"{message} --seed {self.args.seed} has no effect")
+        except AttributeError as e:
+            if args.make or args.all:
+                self.args.seed = self.seedDefault
+        try:
+            check = self.args.nevts
+            if not args.make and not args.all:
+                print(f"{message} --nevts {self.args.nevts} has no effect")
+        except AttributeError as e:
+            if args.make or args.all:
+                self.args.nevts = self.nevtsDefault
+        try:
+            check = self.args.parameterTag
+            if not args.make and not args.all:
+                print(f"{message} --parameterTag {self.args.parameterTag} has no effect")
+        except AttributeError as e:
+            if args.make or args.all:
+                self.args.parameterTag = self.parameterTagDefault
+        try:
+            check = self.args.parameterTagFile
+            if not args.make and not args.all:
+                print(f"{message} --parameterTagFile {self.args.parameterTagFile} has no effect")
+        except AttributeError as e:
+            if args.make or args.all:
+                self.args.parameterTagFile = self.parameterTagFileDefault
+        try:
+            check = self.args.key4hepVersion
+            if not args.make and not args.all:
+                print(f"{message} --key4hepVersion {self.args.key4hepVersion} has no effect")
+        except AttributeError as e:
+            if args.make or args.all:
+                self.args.key4hepVersion = self.key4hepVersionDefault
+
+        # special treatment for nightlies storetrue
+        if not args.make and not args.all and args.key4hepUseNightlies:
+            print(f"k4GeneratorsConfig::WARNING invoking option specific to --make or --all : --key4hepUseNightlies has no effect")
 
 if __name__ == "__main__":
     k4GeneratorsConfig()
