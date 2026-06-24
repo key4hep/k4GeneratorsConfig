4dce41674cd12c6f7bbac3a5c3cd86ca8fb6d273
Dirk Zerwas <dirk.zerwas@in2p3.fr>
2026-06-24 11:14:54 +0200

    add space in distrbution, change format to pdf for better resolution

--- k4GeneratorsConfig/src/eventGenerationCollections2Root.cxx
+++ k4GeneratorsConfig/src/eventGenerationCollections2Root.cxx
@@ -464,7 +464,7 @@ void k4GeneratorsConfig::eventGenerationCollections2Root::writeCrossSectionFigur
     mgRMS->GetYaxis()->SetLabelSize(0.1);
 
     // generate a name and write a png
-    label << m_dirname << "/" << m_processesList[iProc] << "wRMS.png";
+    label << m_dirname << "/" << m_processesList[iProc] << "wRMS.pdf";
     c1->Print(label.str().c_str());
     label.clear();
     label.str("");
@@ -503,7 +503,7 @@ void k4GeneratorsConfig::eventGenerationCollections2Root::writeCrossSectionFigur
     mgDelta->GetYaxis()->SetLabelSize(0.1);
 
     // generate a name and write a png
-    label << m_dirname << "/" << m_processesList[iProc] << "wDelta.png";
+    label << m_dirname << "/" << m_processesList[iProc] << "wDelta.pdf";
     c1->Print(label.str().c_str());
     label.clear();
     label.str("");
@@ -571,7 +571,7 @@ void k4GeneratorsConfig::eventGenerationCollections2Root::writeAnalysisHistosFig
       // now build the legend:
       TLegend* topLegend = new TLegend(0.1, 0.0, 0.4, 0.4);
       std::ostringstream title;
-      title << m_procSqrtsList[proc].first << " #sqrt{s} =" << m_procSqrtsList[proc].second << "GeV";
+      title << m_procSqrtsList[proc].first << " #sqrt{s} =" << m_procSqrtsList[proc].second << " GeV";
       topLegend->SetHeader(title.str().c_str(), "C");
       // we need to get the histo from the top
       TList* topPadPrimitives = topPad->GetListOfPrimitives();
@@ -626,7 +626,7 @@ void k4GeneratorsConfig::eventGenerationCollections2Root::writeAnalysisHistosFig
       // done, save the canvas
       label << m_dirname << "/" << m_procSqrtsList[proc].first
             << (unsigned int)(m_procSqrtsList[proc].second * m_EnergyUnitCnv) << m_cnvAnalysisHistosNames[proc][ihisto]
-            << ".png";
+            << ".pdf";
       m_cnvAnalysisHistos[proc][ihisto]->Print(label.str().c_str());
       label.clear();
       label.str("");
